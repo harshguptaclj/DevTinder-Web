54284c986485265e0612d2460be1790c1936ce0b
Harsh Gupta <harshguptaclj@gmail.com>
2024-10-06 14:02:11 +0530

    built user card on feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { BASE_URL } from '../utils/constants';
+import { addFeed } from '../utils/feedSlice';
+import axios from 'axios';
+import UserCard from './UserCard';
 
 const Feed = () => {
+  const feed = useSelector((store) => store.feed);
+  const dispatch = useDispatch();
+
+  const getFeed = async () =>{
+    if(feed)return;
+    try{
+      const res = await axios.get(BASE_URL + "/feed",{withCredentials: true});
+      dispatch(addFeed(res?.data?.data));
+    } catch(err){
+      // ToDo: handle error
+      console.error(err);
+    }
+  };
+
+  useEffect(()=>{
+    getFeed();
+  },[]);
+
   return (
-    <div>
-      <h1>This is my feed page!</h1>
-    </div>
+    feed && (
+    <div className='flex justify-center my-10'>
+      <UserCard user={feed[0]} />
+    </div>)
   )
 }
 
---
+++ src/components/UserCard.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const UserCard = ({ user }) => {
+    const {firstName, lastName, photUrl, age, gender, about} = user;
+
+  return (
+    <div className="card card-compact bg-base-300 w-96 shadow-xl">
+        <figure>
+            <img
+            src={photUrl}
+            alt="User Photo" />
+        </figure>
+        <div className="card-body">
+            <h2 className="card-title">{firstName + " "+ lastName}</h2>
+            {age && gender && <p>{age +", "+ gender}</p>}
+            <p>{about}</p>
+            <div className="card-actions justify-between">
+                <button className="btn btn-primary">Ignore</button>
+                <button className="btn btn-secondary">Interested</button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default UserCard
